bb3ba5ab1c29b1b19a061e558c367d9a8f14c1a2
slabnevitch <handofschpilrain@mail.ru>
2021-01-14 16:10:02 +0500

    sidebar setup start

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Meetups</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li v-for="(link, ind) in menu" :key="ind">
+            <a href="sass.html">
+             <i class="material-icons left">supervisor_account</i>
+              {{link.text}}
+          </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="sidenav" ref="sidenav" id="mobile-demo">
+       <li v-for="(link, ind) in menu" :key="ind">
+        <a href="sass.html">
+         <i class="material-icons left">supervisor_account</i>
+         {{link.text}}
+       </a>
+     </li>
+   </ul>  
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+    data(){
+    return {
+      menu: [
+        {
+          text: 'Список встреч'
+        }
+      ]
+    }
+  },
+  mounted(){
+    // var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(this.$refs.sidenav, {});
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .nav-wrapper{
+    padding: 0 15px;
+  }
+</style>
